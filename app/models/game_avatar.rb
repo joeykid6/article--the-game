31a5bb97a10ae430ec808fa6c1602a09f1bca785
David Fisher <gogonineties@yahoo.com>
2010-01-27 15:01:21 -0600

    DF: Set paperclip to crop images to exact size.

--- app/models/game_avatar.rb
+++ app/models/game_avatar.rb
@@ -3,7 +3,7 @@ class GameAvatar < ActiveRecord::Base
   belongs_to :games
 
 
-has_attached_file :thumbnail, :styles => {:large=>"121x121>", :medium=>"80x80>", :small=>"30x30>", :tiny=>"12x12>"}
+has_attached_file :thumbnail, :styles => {:large=>"121x121#", :medium=>"80x80#", :small=>"30x30#", :tiny=>"12x12#"}
   validates_attachment_presence :thumbnail
 
 end
--- app/models/media_object.rb
+++ app/models/media_object.rb
@@ -3,7 +3,7 @@ class MediaObject < ActiveRecord::Base
   
   has_and_belongs_to_many :dialogue_lines
 
-  has_attached_file :thumbnail, :styles => {:large=>"121x121>", :medium=>"80x80>", :small=>"30x30>", :tiny=>"12x12>"}
+  has_attached_file :thumbnail, :styles => {:large=>"121x121#", :medium=>"80x80#", :small=>"30x30#", :tiny=>"12x12#"}
   validates_attachment_presence :thumbnail
 
 end
